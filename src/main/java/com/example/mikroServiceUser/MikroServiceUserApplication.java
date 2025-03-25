3f3d9f89764af44ed7a0644edf1dabace8300000
Sasha <Likhanov-2011@yandex.ru>
2025-03-25 16:04:36 +0300

    start project taxiAgregatorUserService

---
+++ src/main/java/com/example/mikroServiceUser/MikroServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mikroServiceUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikroServiceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikroServiceUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mikroServiceUser/MikroServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikroServiceUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikroServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
